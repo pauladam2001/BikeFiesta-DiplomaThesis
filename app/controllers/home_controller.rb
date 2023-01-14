8b8ddecbd09a56235cebce9805ca3a405d32f7b4
pauladam2001 <paul.adrian2001@yahoo.com>
2023-01-14 13:11:28 +0200

    Login and Signup work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if !current_user.present?
+      redirect_to new_user_session_path and return
+    else
+      # redirect_to '#' and return  #TODO
+    end
   end
 end
