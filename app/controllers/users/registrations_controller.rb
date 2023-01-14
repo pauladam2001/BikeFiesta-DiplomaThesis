f4b789d91c0fa51dae6e6a809633fa98fd95b2d9
pauladam2001 <paul.adrian2001@yahoo.com>
2023-01-14 16:54:34 +0200

    Overwrite devise registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,7 +11,31 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    super
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      # clean_up_passwords resource
+      # set_minimum_password_slength
+      # respond_with resource
+      if resource.errors.full_messages.first == "Password is invalid"
+        error_message = "The password needs to be at least 6 characters long, contain a digit, a lowercase letter and an uppercase letter"
+      else
+        error_message = resource.errors.full_messages.first
+      end
+      redirect_to new_user_session_path, alert: "Error - #{error_message}."
+    end
   end
 
   # GET /resource/edit
