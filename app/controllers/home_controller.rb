6ed5db00015fca569f806ee396ba8a5531bb7d9e
pauladam2001 <paul.adrian2001@yahoo.com>
2023-01-15 14:28:17 +0200

    Header for normal user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     if !current_user.present?
       redirect_to new_user_session_path and return
     else
-      # redirect_to '#' and return  #TODO
+      # redirect_to posts_path and return
     end
   end
 end
