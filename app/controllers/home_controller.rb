9be1a47d3b5dd656c3431d981e0a5b2d34e0e1c7
pauladam2001 <paul.adrian2001@yahoo.com>
2023-01-17 23:09:31 +0200

    Redirect path for admin:

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,11 @@ class HomeController < ApplicationController
     if !current_user.present?
       redirect_to new_user_session_path and return
     else
-      redirect_to posts_path and return
+      if current_user.is_normal?
+        redirect_to posts_path and return
+      else
+        #TODO
+      end
     end
   end
 end
