c78967bfc274077649ea5a99c62613767749217d
pauladam2001 <paul.adrian2001@yahoo.com>
2023-01-19 16:16:17 +0200

    Add is_active to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_19_113742) do
+ActiveRecord::Schema.define(version: 2023_01_19_141549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2023_01_19_113742) do
     t.integer "color_id"
     t.integer "location_id"
     t.integer "brandname_id"
+    t.boolean "is_active", default: false
   end
 
   create_table "users", force: :cascade do |t|
