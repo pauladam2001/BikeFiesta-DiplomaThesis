90714a6a8ac14634b1dbd90023743aa9b095c4c2
pauladam2001 <paul.adrian2001@yahoo.com>
2023-02-02 13:07:55 +0200

    Password recovery works

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,9 +35,22 @@ Rails.application.configure do
   config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: ENV['GOOGLE_ACCOUNT']}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            ENV['GOOGLE_ACCOUNT'],
+  password:             ENV['GOOGLE_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+  config.action_mailer.default_url_options = {:host =>'localhost:3002'}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
