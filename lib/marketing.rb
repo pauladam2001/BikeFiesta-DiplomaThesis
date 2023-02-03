79b6ab2d939ee079b6c5a8da41d38f6416d43935
pauladam2001 <paul.adrian2001@yahoo.com>
2023-02-03 11:19:52 +0200

    Add comments

--- lib/marketing.rb
+++ lib/marketing.rb
@@ -1,5 +1,8 @@
 module Marketing
   
+  # Called every time a post is created (async)
+  # This function checks if there is a bike photo in the images associated with a post and after that it makes the post
+  # active and sends an SMS for the followers of the user
   def self.check_images_for_post(post_id)
     post = Post.find(post_id)
     link = 'http://localhost:8000/api/predict?token=24apa2001&cloudinary_url='
@@ -35,6 +38,7 @@ module Marketing
   end
 
   # Called hourly
+  # Delete any posts from db that have no associated images and are created more than 30 miutes ago
   def self.check_posts_without_assets
     Post.find_each do |post|
       if post.assets.count < 0 && post.created_at < 30.minutes.ago
@@ -44,6 +48,7 @@ module Marketing
   end
 
   # Called daily
+  # Mark random posts for sale
   def self.mark_posts_on_sale
     random_offset = Random.rand(Post.count)
     random_posts = Post.offset(random_offset).first(Post.count / 3)
@@ -63,6 +68,7 @@ module Marketing
   end
 
   # Called daily
+  # Unmark the posts from sale
   def self.unmark_posts_on_sale
     Post.where("sale_price_expiration <= ?", Time.now).where.not(sale_price: nil).find_each do |post|
       post.sale = nil
@@ -73,6 +79,7 @@ module Marketing
   end
 
   # Called every 30 minutes
+  # Reset codes for 2FA
   def self.reset_user_codes
     User.where("code_expiration_date <= ?", Time.now).find_each do |user|
       user.code = nil
