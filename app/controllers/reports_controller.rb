23eab62686d3abc5a4947367f6b8615a913e5240
pauladam2001 <paul.adrian2001@yahoo.com>
2023-02-11 14:44:30 +0200

    Solved filter for reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,6 +5,12 @@ class ReportsController < ApplicationController
   def index
     @reports = Report.all
 
+    if params[:solved].present?
+      @reports = @reports.where(solved: params[:solved])
+    else
+      @reports = @reports.where(solved: false)
+    end
+
     start_date, end_date = nil, nil
     start_date = params[:start_date].to_date.beginning_of_day if params[:start_date].present? && params[:start_date] != "all"
     end_date = params[:end_date].to_date.end_of_day if params[:end_date].present? && params[:end_date] != "all"
