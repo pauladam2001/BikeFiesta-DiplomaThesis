82835b9a34164abb8d50375344d578e12af757b2
pauladam2001 <paul.adrian2001@yahoo.com>
2023-02-14 13:51:27 +0200

    Add todos

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -3,5 +3,7 @@ class PurchaseController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    # TODO transform params[:county] back from short to name
+    # TODO check that CVV is 3 digits and card number is 16 digits
   end
 end
