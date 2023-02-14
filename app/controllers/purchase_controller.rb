73424047e03d1a1f7dcf17ac7fb09df7e3efddcb
pauladam2001 <paul.adrian2001@yahoo.com>
2023-02-14 14:26:00 +0200

    Small fixes

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,7 +1,7 @@
 class PurchaseController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
-  
+
   def index
     # TODO transform params[:county] back from short to name
     # TODO check that CVV is 3 digits and card number is 16 digits
