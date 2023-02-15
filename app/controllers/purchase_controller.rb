2b94ad550eea112a2298090aefe8283e58c17841
pauladam2001 <paul.adrian2001@yahoo.com>
2023-02-15 09:14:35 +0200

    Check card number

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -3,7 +3,13 @@ class PurchaseController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    if params[:submitButton] == "Complete Purchase"
+      card_number = params[:card_number]
+      card_number = card_number.delete(' ')
+      if card_number != 16
+        redirect_back(fallback_location: checkout_path(post_id: params[:post_id]), alert: "Error - The card number needs to have exactly 16 digits.")
+      end
+    end
     # TODO transform params[:county] back from short to name
-    # TODO check that card number is 16 digits
   end
 end
