bd321ee521f8aea9565c17163d62c2df59a54c88
pauladam2001 <paul.adrian2001@yahoo.com>
2023-03-07 14:34:17 +0200

    Reviews page for admin and turbo fixes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,32 @@ class ReviewsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
 
+  def index
+    if current_user.is_normal?
+      redirect_to posts_path
+      return
+    end
+
+    @reviews = Review.order(created_at: :desc)
+
+    @users = User.order(:full_name).pluck(:full_name, :id)
+
+    @reviews = @reviews.where("message ilike?", "%#{params[:message]}%") if params[:message].present?
+    @reviews = @reviews.where(grade: params[:grade].to_i) if params[:grade].present?
+
+    start_date, end_date = nil, nil
+    start_date = params[:start_date].to_date.beginning_of_day if params[:start_date].present? && params[:start_date] != "all"
+    end_date = params[:end_date].to_date.end_of_day if params[:end_date].present? && params[:end_date] != "all"
+
+    @reviews = @reviews.where("reviews.created_at >= ?", start_date) if params[:start_date].present? && params[:start_date] != "all"
+    @reviews = @reviews.where("reviews.created_at <= ?", end_date) if params[:end_date].present? && params[:end_date] != "all"
+
+    @reviews = @reviews.where(reviewer_id: params[:reviewer]) if params[:reviewer].present? && params[:reviewer] != "all"
+    @reviews = @reviews.where(reviewed_id: params[:reviewed]) if params[:reviewed].present? && params[:reviewed] != "all"
+
+    @reviews = @reviews.paginate(page: params[:page], per_page: 15)
+  end
+
   def new
     @review = Review.new
 
@@ -26,5 +52,11 @@ class ReviewsController < ApplicationController
 
       redirect_to posts_path
     end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
   end
 end
