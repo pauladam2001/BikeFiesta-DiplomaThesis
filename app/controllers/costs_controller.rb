75f02c59683b727b379afdfbe8e4777cefacc85a
pauladam2001 <paul.adrian2001@yahoo.com>
2023-03-09 14:52:49 +0200

    Costs filters

--- app/controllers/costs_controller.rb
+++ app/controllers/costs_controller.rb
@@ -5,6 +5,15 @@ class CostsController < ApplicationController
   def index
     @costs = Cost.all
 
+    @costs = @costs.where("description ilike ?", "%#{params[:description]}%") if params[:description].present?
+
+    start_date, end_date = nil, nil
+    start_date = params[:start_date].to_date.beginning_of_day if params[:start_date].present? && params[:start_date] != "all"
+    end_date = params[:end_date].to_date.end_of_day if params[:end_date].present? && params[:end_date] != "all"
+
+    @costs = @costs.where("costs.created_at >= ?", start_date) if params[:start_date].present? && params[:start_date] != "all"
+    @costs = @costs.where("costs.created_at <= ?", end_date) if params[:end_date].present? && params[:end_date] != "all"
+
     @costs = @costs.paginate(page: params[:page], per_page: 12)
   end
 
