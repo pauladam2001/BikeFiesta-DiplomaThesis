18715c58b36236f9eb443fcc11c57964cb4feaee
pauladam2001 <paul.adrian2001@yahoo.com>
2023-04-20 17:18:31 +0300

    Delete cron job for chat notifications

--- app/workers/cron_job_send_chat_notification_to_users.rb
+++
@@ -1,9 +0,0 @@
-class CronJobSendChatNotificationToUsers                      
-  include Sidekiq::Worker
-  sidekiq_options queue: "default"
-  def perform()                     
-    puts "Running CronJobSendChatNotificationToUsers"
-    Marketing.send_chat_notification_to_users
-    puts "Finish Running CronJobSendChatNotificationToUsers"
-  end                     
-end
